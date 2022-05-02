da6a2558ba1c5954348ddb251934bc673c7fbb5e
AsierBreyas <javiasterio@gmail.com>
2022-05-02 12:24:21 +0200

    Cors arreglado

--- Startup.cs
+++ Startup.cs
@@ -42,8 +42,11 @@ namespace CatalogoJuegosApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors(options => options
-                                    .AllowAnyOrigin());
+            app.UseCors(x => x
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .SetIsOriginAllowed(origin => true) // allow any origin
+               .AllowCredentials());
 
             if (env.IsDevelopment())
             {
